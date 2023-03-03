db75e66635a4345779306fddfb19595e94dc5e84
zxcvbnma702 <83083486+zxcvbnma702@users.noreply.github.com>
2023-03-04 00:58:48 +0800

    home 1

--- app/src/main/java/com/example/servicecreate/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/servicecreate/ui/home/HomeFragment.kt
@@ -2,10 +2,24 @@ package com.example.servicecreate.ui.home
 
 import com.example.base.ui.activity.BaseFragment
 import com.example.servicecreate.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayout
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override fun FragmentHomeBinding.initBindingView() {
 
+        homeTablayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
     }
 
 }
